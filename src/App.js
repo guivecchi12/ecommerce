a72645cab28cd49748f3c5c26cbe00c0d84593a9
Guilherme-Vecchi <guivecchi12@gmail.com>
2021-09-02 13:34:35 -0600

    front end connected

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+    return(
+        <div>
+            FRONT END
+        </div>
+    )
+}
+
+export default App;
