a23939ea63021554905fd664548b61ef673446de
Guilherme-Vecchi <guivecchi12@gmail.com>
2021-09-24 14:45:35 -0600

    header

--- client/frontend/src/App.js
+++ client/frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      {/* <LandingPage/> */}
+      <LandingPage/>
     </div>
   );
 }
--- client/frontend/src/component/landingPage.js
+++ client/frontend/src/component/landingPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
 // import { useEffect, useState } from 'react';
+import {Button} from 'react-bootstrap';
+import "../CSS/landingPage.css";
 
 const LandingPage = () => {
     return(
-        <div> Landing Pages </div>
+        <div className="home"> 
+            <h4>Welcome to my Ecommerce</h4>
+            <Button variant="primary">Continue as Guest</Button>
+        </div>
+
     )
 }
 
